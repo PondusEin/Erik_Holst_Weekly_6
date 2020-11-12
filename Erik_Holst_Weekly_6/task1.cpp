d5f7d1efe7a3c82b4920907b0d57fc45ae654f30
PondusEin <69846011+PondusEin@users.noreply.github.com>
2020-11-12 23:46:27 +0100

    started task 2.

--- Erik_Holst_Weekly_6/task1.cpp
+++ Erik_Holst_Weekly_6/task1.cpp
@@ -1,4 +1,24 @@
 #include "menu.h"
+
+
+
 void task1()
 {
+	std::string passwordString;
+	system("cls");
+	std::cout << "The password is:" << std::endl;
+	std::string wifiPassword = passwordString;
+	do
+	{
+		std::cin.clear();
+		std::cin.ignore();
+		std::getline(std::cin, passwordString);
+	} while (std::cin.fail());
+	std::ifstream secretBaby("Baby.txt");
+	if (!secretBaby.eof())
+	{
+		
+	}
+	secretBaby.close();
+	system("pause");
 }
--- Erik_Holst_Weekly_6/task2.cpp
+++ Erik_Holst_Weekly_6/task2.cpp
@@ -1,6 +1,15 @@
 #include "menu.h"
+class MyClass
+{
+public:
+	std::string studentsName;
+	int studentScores;
+	std::string results;
+};
+
 void task2()
 {
+	system("cls");
 	//make files with studentsa and score name?
 	//get character --> see if first position is A. If true, ++grade. else, nothing and read next character. etc. 
 }
