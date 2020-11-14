fdf2b0037e9c0fc7fc767dbdbebecf700fd96e20
PondusEin <69846011+PondusEin@users.noreply.github.com>
2020-11-14 01:01:25 +0100

    started on task 2.

--- Erik_Holst_Weekly_6/task2.cpp
+++ Erik_Holst_Weekly_6/task2.cpp
@@ -1,15 +1,37 @@
 #include "menu.h"
-class MyClass
-{
+#include <iomanip>
+
+class Student{
 public:
 	std::string studentsName;
 	int studentScores;
 	std::string results;
 };
 
+void calculateGrade() {
+
+}
+double average() {
+
+}
+int max() {
+
+}
+int min() {
+
+}
 void task2()
 {
 	system("cls");
-	//make files with studentsa and score name?
+
+	std::cout << "Studentname\tScore\n";
+
+	std::cout << "Number of students: " << std::endl;
+	std::cout << "Average score: " << std::setprecision(3) << /*average(students)*/ std::endl;
+	std::cout << "Lowerst score: " << /*max(students)*/std::endl;
+	std::cout << "Highest score: " << /*min(students)*/ std::endl;
+
+	system("pause");
+ 	//make files with studentsa and score name?
 	//get character --> see if first position is A. If true, ++grade. else, nothing and read next character. etc. 
 }
